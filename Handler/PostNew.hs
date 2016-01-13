c7616d7e9f6b58dea3f22227f89b0bf786068798
ISANKA <ISANKA@tosa-no-Mac-Pro-3.local>
2016-01-13 19:52:25 +0900

    blog complete

--- Handler/PostNew.hs
+++ Handler/PostNew.hs
@@ -2,18 +2,28 @@ module Handler.PostNew where
 
 import Import
 import Yesod.Form.Bootstrap3
+--import Yesod.Form.Nic (YesodNic, nicHtmlField)
+import Yesod.Text.Markdown
 
-data BlogPost = BlogPost
-	{title :: Text
-	}
 
-blogPostForm :: AForm Handler BlogPost
+blogPostForm :: AForm Handler BlogPost 
 blogPostForm = BlogPost 
-			<$> areq textField (bfs ("Title" :: Text)) Nothing
-
+            <$> areq textField (bfs ("Title" :: Text)) Nothing
+            <*> areq textField (bfs ("Tags" :: Text)) Nothing
+           -- <*> areq textareaField ("Content" :: Text) Nothing
+            <*> areq markdownField (bfs ("Article" :: Text)) Nothing
 
 getPostNewR :: Handler Html
-getPostNewR = do 
-	(widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
-	defaultLayout $ do
-		$(widgetFile "post/new")
+getPostNewR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+    defaultLayout $ do
+        $(widgetFile "posts/new")
+
+postPostNewR :: Handler Html
+postPostNewR = do
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+    case res of
+      FormSuccess blogPost -> do
+              blogPostId <- runDB $ insert blogPost
+              redirect $ PostDetailsR blogPostId
+      _ -> defaultLayout $(widgetFile "posts/new") 
