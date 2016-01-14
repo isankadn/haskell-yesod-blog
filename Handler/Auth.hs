a5baa01a56c78e9934c9a9d71868b8c0132de2e2
isankadn <isankadn@gmail.com>
2016-01-14 12:02:17 +0000

    bug

---
+++ Handler/Auth.hs
@@ -0,0 +1,48 @@
+module Handler.Auth where
+
+import Import
+
+import           Data.Default                (def)
+import           Data.Text                   (Text)
+import           Network.HTTP.Client.Conduit (Manager, newManager)
+import           Yesod
+import           Yesod.Auth
+import           Yesod.Auth.BrowserId
+import           Yesod.Auth.GoogleEmail
+
+data App = App
+    { httpManager :: Manager
+    }
+
+instance Yesod App where
+    -- Note: In order to log in with BrowserID, you must correctly
+    -- set your hostname here.
+    approot = ApprootStatic "http://localhost:3000"
+
+instance YesodAuth App where
+    type AuthId App = Text
+    getAuthId = return . Just . credsIdent
+
+    loginDest _ = HomeR
+    logoutDest _ = HomeR
+
+    authPlugins _ =
+        [ authBrowserId def
+        , authGoogleEmail
+        ]
+
+    authHttpManager = httpManager
+
+    -- The default maybeAuthId assumes a Persistent database. We're going for a
+    -- simpler AuthId, so we'll just do a direct lookup in the session.
+    maybeAuthId = lookupSession "_ID"
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+getHomeR :: Handler Html
+getHomeR = do
+    maid <- maybeAuthId
+    defaultLayout $ do
+        $(widgetFile "login") 
+        
---
+++ test/Handler/AuthSpec.hs
@@ -0,0 +1,14 @@
+module Handler.AuthSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getAuthR" $ do
+        error "Spec not implemented: getAuthR"
+
+
+    describe "postAuthR" $ do
+        error "Spec not implemented: postAuthR"
+
